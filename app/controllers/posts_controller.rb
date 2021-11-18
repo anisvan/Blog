a36cef209c65759d38d53513023eb45e0b24e9f1
Axel06 <Anisvan@mail.ru>
2021-11-18 18:06:06 +0600

    added admin panel

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
 	
-	before_action :authenticate_user!, except:[:index, :show]
+	
 
-	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :set_post, only: [:show]
 
 	def index
 		@posts = Post.paginate(page: params[:page], per_page: 4)
@@ -12,47 +12,12 @@ class PostsController < ApplicationController
 		
 	end
 
-	def new
-		@post = Post.new
-	end
-
-	def create
-		@post = Post.new(post_params)
-		if @post.save
-			redirect_to @post, info: 'Статья успешно создана'
-		else
-			render :new, danger: 'Статья не создана'
-		end
-	end
-
-	def edit
-		
-	end
-
-	def update
-		
-		if @post.update(post_params)
-			
-			redirect_to @post,  success: 'Статья успешно обновлена'
-
-		else
-			render :edit, danger: 'Статья не обновлена'
-		end
-	end
-
-	def destroy
-		
-		@post.destroy
-		redirect_to posts_path, success: 'Статья успешно удалена'
-	end
-
+	
 	private
 
 	def set_post
 		@post = Post.find(params[:id])
 	end
 
-	def post_params
-		params.require(:post).permit(:title, :summary, :body, :image, :content,:all_tags)
-	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :posts
+  resources :posts, only: [:show, :index]
   resources :tags, only: [:show]
+
+  namespace :admin do
+    resources :posts, except: [:show, :index]
+    resources :pictures, only: [:create, :destroy] 
+  end
+
 end
+
